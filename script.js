e180947920e71f324fb176250b5803e621c92805
Anderson Maia <andersonmaiacode@gmail.com>
2023-03-13 15:28:36 -0300

    Finished tutorial part

--- script.js
+++ script.js
@@ -22,4 +22,52 @@ function getResults(query) {
 
 function displayResults(weather) {
   console.log(weather);
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+  let weatherEl = document.querySelector(".current .weather");
+  weatherEl.innerText = weather.weather[0].main;
+
+  let hiLow = document.querySelector(".current .hi-low");
+  hiLow.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`;
+}
+
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "may",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`;
 }
