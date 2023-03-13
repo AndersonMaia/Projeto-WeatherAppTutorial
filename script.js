e6aea465ffe776cd2c1d9fbb86410a839e8bb06e
Anderson Maia <andersonmaiacode@gmail.com>
2023-03-13 15:01:24 -0300

    Started Working on the JS code. Added more BG images.

---
+++ script.js
@@ -0,0 +1,25 @@
+const api = {
+  key: "024aa483e6a916a32ff0ea962181a68b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(event) {
+  if (event.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
+}
